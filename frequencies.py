ccc8d5aa27058eae11603b973ffd13913d047f1a
Augusto Favero <augusto.favero@kcl.ac.uk>
2022-10-07 17:56:35 +0100

    Completed frequencies version

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,15 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for value in items:
+        value = str(value)
+        if value in frequencies.keys():
+            frequencies[value] += 1
+        else:
+            frequencies[value] = 1
     return frequencies
+
+
+#frequencies(['a','a','b','b','b','c'])
+#frequencies([])
+#frequencies([100, 'Hello', '100', '100', 100])
